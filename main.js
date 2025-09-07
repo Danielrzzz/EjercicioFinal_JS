1ae47f791593c0dd173e8491fe300c45897649c7
Danielrzzz <daniel.rodriguez2999@gmail.com>
2025-09-07 18:49:06 +0200

    BasicoHTML_productosJS

---
+++ main.js
@@ -0,0 +1,102 @@
+const products = [
+  {
+    name: 'Aceite de Aguacate corporal 125ml',
+    price: 16.85,
+    stars: 4.8,
+    reviews: 829,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '21100009',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/8/4/8432430006482.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite esencial de Manzanilla 12ml',
+    price: 16.15,
+    stars: 4.2,
+    reviews: 340,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '13104452',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/8/4/8432430005805.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite de Aguacate 250ml',
+    price: 12.05,
+    stars: 4.6,
+    reviews: 300,
+    seller: 'Natures Oils',
+    brand: 'Olivado',
+    Referencia: '99700990',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/a/c/aceite-de-aguacate-250ml-.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite esencial de Naranja 12ml',
+    price: 11.05,
+    stars: 4.3,
+    reviews: 130,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '32401018',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/8/4/8432430010540.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite esencial de Limón ecológico 30ml',
+    price: 13.49,
+    stars: 4.0,
+    reviews: 180,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '99706143',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/8/4/8432430106724.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite de almendras dulces 500ml',
+    price: 12.99,
+    stars: 4.9,
+    reviews: 800,
+    seller: 'Natures Oils',
+    brand: 'Terra Verda',
+    Referencia: '32304045',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/3/2/32304045_1.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Complejo de Aceites de Espino Amarillo 12ml',
+    price: 19.99,
+    stars: 4.8,
+    reviews: 1233,
+    seller: 'Natures Oils',
+    brand: 'Natura Siberica',
+    Referencia: '99713242',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/a/c/aceite_espinoamarillo_4744183010192_b.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite esencial de Romero ecológico 30ml',
+    price: 19.99,
+    stars: 5.0,
+    reviews: 450,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '99706141',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/8/4/8432430106748.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite de zanahoria corporal 125ml',
+    price: 16.99,
+    stars: 4.5,
+    reviews: 233,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '99709006',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/a/c/aceite-de-zanahoria-125ml.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  },
+  {
+    name: 'Aceite esencial de Repelente de Insectos 12ml',
+    price: 7.99,
+    stars: 4.5,
+    reviews: 185,
+    seller: 'Natures Oils',
+    brand: 'Bio Cesta',
+    Referencia: '32401028',
+    image: 'https://www.herbolarionavarro.es/media/catalog/product/8/4/8432430010649.jpg?quality=80&bg-color=255,255,255&fit=bounds&height=1024&width=1024&canvas=1024:1024'
+  }
+];
